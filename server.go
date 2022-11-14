a80ed0f69683adb1ee05eceac0940b484008d63d
JenisBen <benjamin199551@gmail.com>
2022-11-14 12:35:08 -0800

    initial commit

---
+++ server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+    })
+
+    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Hi")
+    })
+
+	fmt.Println("Server running on 5000")
+    log.Fatal(http.ListenAndServe(":5000", nil))
+}
